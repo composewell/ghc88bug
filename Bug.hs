dbd0f3493909ace36702ebbbc88b487a3e1f1eee
Harendra Kumar <harendra.kumar@gmail.com>
2020-01-28 23:51:21 +0530

    remove the Stream module

--- Bug.hs
+++ Bug.hs
@@ -1,22 +1,93 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE RankNTypes                #-}
 
 module Bug
     ( fromStreamVar
     , SVar(..)
     , ChildEvent(..)
+    , drain
     )
 where
 
-import Control.Exception (fromException)
-import Control.Monad.Catch (throwM)
-
 import Control.Concurrent (ThreadId)
+import Control.Exception (fromException)
 import Control.Exception (SomeException(..), Exception)
+import Control.Monad.Catch (throwM)
 import Control.Monad.Catch (MonadThrow)
 import Control.Monad.IO.Class (MonadIO(..))
+import Control.Monad.Trans.Class (MonadTrans(lift))
 import Control.Monad.Trans.Control (MonadBaseControl)
-import Stream (Stream, mkStream, foldStream)
+import Prelude hiding (map, mapM, concatMap, foldr)
+
+newtype Stream m a =
+    MkStream (forall r.
+              (a -> Stream m a -> m r) -- yield
+            -> (a -> m r)               -- singleton
+            -> m r                      -- stop
+            -> m r
+            )
+
+{-# INLINE [2] mkStream #-}
+mkStream :: IsStream t
+    => (forall r. (a -> t m a -> m r)
+        -> (a -> m r)
+        -> m r
+        -> m r)
+    -> t m a
+mkStream k = fromStream $ MkStream $ \yld sng stp ->
+    let yieldk a r = yld a (toStream r)
+     in k yieldk sng stp
+
+{-# INLINE [1] yieldM #-}
+yieldM :: (Monad m) => m a -> Stream m a
+yieldM m = MkStream $ \_ single _ -> m >>= single
+
+{-# INLINE foldStream #-}
+foldStream
+    :: IsStream t
+    => (a -> t m a -> m r)
+    -> (a -> m r)
+    -> m r
+    -> t m a
+    -> m r
+foldStream yld sng stp m =
+    let yieldk a x = yld a (fromStream x)
+        MkStream k = toStream m
+     in k yieldk sng stp
+
+{-# INLINE map #-}
+map :: (a -> b) -> Stream m a -> Stream m b
+map f xs = go xs
+    where
+      go m = MkStream $ \yld sng stp ->
+                 let yieldk a r = yld (f a) (go r)
+                     single a   = sng (f a)
+                 in  foldStream yieldk single stp m
+{-# INLINE [1] drain #-}
+drain :: (Monad m, IsStream t) => t m a -> m ()
+drain m = go m
+    where
+    go m1 =
+        let stop = return ()
+            single _ = return ()
+            yieldk _ r = go r
+        in foldStream yieldk single stop m1
+
+
+instance Monad m => Functor (Stream m) where
+    fmap = map
+
+instance MonadTrans Stream where
+    lift = yieldM
+
+class IsStream t where
+    toStream :: t m a -> Stream m a
+    fromStream :: Stream m a -> t m a
+
+instance IsStream Stream where
+    toStream = id
+    fromStream = id
 
 data ThreadAbort = ThreadAbort deriving Show
 
--- Main.hs
+++ Main.hs
@@ -1,10 +1,9 @@
 module Main (main) where
 
-import qualified Stream as Stream
 import qualified Bug as Bug
 
 main :: IO ()
-main = Stream.drain $ Bug.fromStreamVar infiniteStream
+main = Bug.drain $ Bug.fromStreamVar infiniteStream
 
 infiniteStream :: Bug.SVar IO Int
 infiniteStream = Bug.SVar $ return $ fmap Bug.ChildYield [1]
